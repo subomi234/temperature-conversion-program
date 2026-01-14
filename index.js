b7cdddf26555b1951184f2f9b6e9368460a5dd80
Subomi Osoba <subomi234osoba@gmail.com>
2026-01-14 18:42:13 -0500

    basic functionality

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+const textBox = document.getElementById("textBox");
+const toF = document.getElementById("toF");
+const toC = document.getElementById("toC");
+const result = document.getElementById("result");
 
+let temp;
 
 function convert() {
-
+    if (toF.checked) {
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed() + "F";
+    }
+    else if (toC.checked) {
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5 / 9);
+        result.textContent = temp.toFixed() + "C";
+    }
+    else {
+        result.textContent = "Select a result";
+    }
 }
